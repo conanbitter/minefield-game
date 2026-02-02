dcb513abfbc40a25bd8a6c7dad9c24ef978b9bf7
Conan Bitter <conan.bitter@gmail.com>
2026-02-02 21:04:08 +0300

    Refactor button faces

--- include/atlas.h
+++ include/atlas.h
@@ -3,6 +3,11 @@
 
 #include "grf.h"
 
+typedef struct ButtonFace {
+    const GRFRect* normal;
+    const GRFRect* pressed;
+} ButtonFace;
+
 const GRFRect CELL_DIGITS[9];
 const GRFRect CELL_MINE;
 const GRFRect CELL_MINE_OTHER;
@@ -12,13 +17,16 @@ const GRFRect CELL_FLAG;
 const GRFRect CELL_MARK;
 const GRFRect CELL_MARK_DOWN;
 
-const GRFRect DIFFICULTY_EASY[2];
-const GRFRect DIFFICULTY_MEDIUM[2];
-const GRFRect DIFFICULTY_HARD[2];
+const ButtonFace DIFFICULTY_EASY;
+const ButtonFace DIFFICULTY_EASY_ACTIVE;
+const ButtonFace DIFFICULTY_MEDIUM;
+const ButtonFace DIFFICULTY_MEDIUM_ACTIVE;
+const ButtonFace DIFFICULTY_HARD;
+const ButtonFace DIFFICULTY_HARD_ACTIVE;
 
-const GRFRect SMILE_NORMAL[2];
-const GRFRect SMILE_DEAD[2];
-const GRFRect SMILE_WIN[2];
+const ButtonFace SMILE_NORMAL;
+const ButtonFace SMILE_DEAD;
+const ButtonFace SMILE_WIN;
 const GRFRect SMILE_OPEN;
 
 const GRFRect COUNTER_DIGITS[10];
--- src/atlas.c
+++ src/atlas.c
@@ -22,43 +22,69 @@ const GRFRect CELL_FLAG = { .x = 1, .y = 403, .w = 48, .h = 48 };
 const GRFRect CELL_MARK = { .x = 246, .y = 452, .w = 48, .h = 48 };
 const GRFRect CELL_MARK_DOWN = { .x = 50, .y = 354, .w = 48, .h = 48 };
 
-const GRFRect DIFFICULTY_EASY[2] = {
-    {.x = 1, .y = 285, .w = 120, .h = 58},
-    {.x = 243, .y = 167, .w = 120, .h = 58},
-};
-const GRFRect DIFFICULTY_EASY_ACTIVE[2] = {
-    {.x = 1, .y = 226, .w = 120, .h = 58},
-    {.x = 1, .y = 167, .w = 120, .h = 58},
-};
-const GRFRect DIFFICULTY_MEDIUM[2] = {
-    {.x = 1, .y = 108, .w = 120, .h = 58},
-    {.x = 220, .y = 344, .w = 120, .h = 58},
-};
-const GRFRect DIFFICULTY_MEDIUM_ACTIVE[2] = {
-    {.x = 122, .y = 226, .w = 120, .h = 58},
-    {.x = 99, .y = 344, .w = 120, .h = 58},
-};
-const GRFRect DIFFICULTY_HARD[2] = {
-    {.x = 243, .y = 226, .w = 120, .h = 58},
-    {.x = 243, .y = 285, .w = 120, .h = 58},
-};
-const GRFRect DIFFICULTY_HARD_ACTIVE[2] = {
-    {.x = 122, .y = 167, .w = 120, .h = 58},
-    {.x = 122, .y = 285, .w = 120, .h = 58},
+static const GRFRect DIFFICULTY_EASY_NORMAL = { .x = 1, .y = 285, .w = 120, .h = 58 };
+static const GRFRect DIFFICULTY_EASY_PRESSED = { .x = 243, .y = 167, .w = 120, .h = 58 };
+const ButtonFace DIFFICULTY_EASY = {
+    .normal = &DIFFICULTY_EASY_NORMAL,
+    .pressed = &DIFFICULTY_EASY_PRESSED
 };
 
-const GRFRect SMILE_NORMAL[2] = {
-    {.x = 1, .y = 25, .w = 82, .h = 82},
-    {.x = 167, .y = 1, .w = 82, .h = 82},
+static const GRFRect DIFFICULTY_EASY_ACTIVE_NORMAL = { .x = 1, .y = 226, .w = 120, .h = 58 };
+static const GRFRect DIFFICULTY_EASY_ACTIVE_PRESSED = { .x = 1, .y = 167, .w = 120, .h = 58 };
+const ButtonFace DIFFICULTY_EASY_ACTIVE = {
+    .normal = &DIFFICULTY_EASY_ACTIVE_NORMAL,
+    .pressed = &DIFFICULTY_EASY_ACTIVE_PRESSED
 };
-const GRFRect SMILE_DEAD[2] = {
-    {.x = 122, .y = 84, .w = 82, .h = 82},
-    {.x = 250, .y = 1, .w = 82, .h = 82},
+
+static const GRFRect DIFFICULTY_MEDIUM_NORMAL = { .x = 1, .y = 108, .w = 120, .h = 58 };
+static const GRFRect DIFFICULTY_MEDIUM_PRESSED = { .x = 220, .y = 344, .w = 120, .h = 58 };
+const ButtonFace DIFFICULTY_MEDIUM = {
+    .normal = &DIFFICULTY_MEDIUM_NORMAL,
+    .pressed = &DIFFICULTY_MEDIUM_PRESSED
 };
-const GRFRect SMILE_WIN[2] = {
-    {.x = 205, .y = 84, .w = 82, .h = 82},
-    {.x = 288, .y = 84, .w = 82, .h = 82},
+
+static const GRFRect DIFFICULTY_MEDIUM_ACTIVE_NORMAL = { .x = 122, .y = 226, .w = 120, .h = 58 };
+static const GRFRect DIFFICULTY_MEDIUM_ACTIVE_PRESSED = { .x = 99, .y = 344, .w = 120, .h = 58 };
+const ButtonFace DIFFICULTY_MEDIUM_ACTIVE = {
+    .normal = &DIFFICULTY_MEDIUM_ACTIVE_NORMAL,
+    .pressed = &DIFFICULTY_MEDIUM_ACTIVE_PRESSED
 };
+
+static const GRFRect DIFFICULTY_HARD_NORMAL = { .x = 243, .y = 226, .w = 120, .h = 58 };
+static const GRFRect DIFFICULTY_HARD_PRESSED = { .x = 243, .y = 285, .w = 120, .h = 58 };
+const ButtonFace DIFFICULTY_HARD = {
+    .normal = &DIFFICULTY_HARD_NORMAL,
+    .pressed = &DIFFICULTY_HARD_PRESSED
+};
+
+static const GRFRect DIFFICULTY_HARD_ACTIVE_NORMAL = { .x = 122, .y = 167, .w = 120, .h = 58 };
+static const GRFRect DIFFICULTY_HARD_ACTIVE_PRESSED = { .x = 122, .y = 285, .w = 120, .h = 58 };
+const ButtonFace DIFFICULTY_HARD_ACTIVE = {
+    .normal = &DIFFICULTY_HARD_ACTIVE_NORMAL,
+    .pressed = &DIFFICULTY_HARD_ACTIVE_PRESSED
+};
+
+static const GRFRect SMILE_NORMAL_NORMAL = { .x = 1, .y = 25, .w = 82, .h = 82 };
+static const GRFRect SMILE_NORMAL_PRESSED = { .x = 167, .y = 1, .w = 82, .h = 82 };
+const ButtonFace SMILE_NORMAL = {
+    .normal = &SMILE_NORMAL_NORMAL,
+    .pressed = &SMILE_NORMAL_PRESSED
+};
+
+static const GRFRect SMILE_DEAD_NORMAL = { .x = 122, .y = 84, .w = 82, .h = 82 };
+static const GRFRect SMILE_DEAD_PRESSED = { .x = 250, .y = 1, .w = 82, .h = 82 };
+const ButtonFace SMILE_DEAD = {
+    .normal = &SMILE_DEAD_NORMAL,
+    .pressed = &SMILE_DEAD_PRESSED
+};
+
+static const GRFRect SMILE_WIN_NORMAL = { .x = 205, .y = 84, .w = 82, .h = 82 };
+static const GRFRect SMILE_WIN_PRESSED = { .x = 288, .y = 84, .w = 82, .h = 82 };
+const ButtonFace SMILE_WIN = {
+    .normal = &SMILE_WIN_NORMAL,
+    .pressed = &SMILE_WIN_PRESSED
+};
+
 const GRFRect SMILE_OPEN = { .x = 84, .y = 1, .w = 82, .h = 82 };
 
 const GRFRect COUNTER_DIGITS[10] = {
